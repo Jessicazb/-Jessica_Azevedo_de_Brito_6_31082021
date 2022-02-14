165eeb047721445163a6b16a8a5f71806c181881
Jéssica Azevedo <jessica.azdo@gmail.com>
2022-02-14 18:16:47 +0100

    ajout du projet 6

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -5,21 +5,23 @@ const sauce = require('../models/sauce');
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
-  const sauce = new Sauce({...sauceObject,
-  imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
   sauce.save()
-    .then(() => res.status(201).json({ message: 'Sauce enregistrée!'}))
+    .then(() => res.status(201).json({ message: 'Sauce enregistrée!' }))
     .catch(error => res.status(400).json({ error }));
 };
 
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ?
-    {...JSON.parse(req.body.sauce),
+    {
+      ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
     .catch(error => res.status(400).json({ error }));
 };
 
@@ -29,7 +31,7 @@ exports.deleteSauce = (req, res, next) => {
       const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
+          .then(() => res.status(200).json({ message: 'Sauce supprimé !' }))
           .catch(error => res.status(400).json({ error }));
       });
     })
@@ -37,13 +39,48 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
+  Sauce.findOne({ _id: req.params.id })
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({ error }));
 };
 
 exports.getAllSauce = (req, res, next) => {
-    Sauce.find()
+  Sauce.find()
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({ error }));
-  }
+}
+
+exports.likesDeslikes = (req, res, next) => {
+  // likes = 1 (likes = +1)
+  // likes = -1 (deslikes = +1)
+  // likes = 0 ( deslikes = 0)
+
+  // chercher l'objet dans la base do donnée
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      //res.status(200).json(sauce);
+      if (!sauce.usersLiked.includes(req.body.userId) && req.body.like === 1) {
+        console.log("userId n'est pas dans usersLiked dans la base de donnée et requete front like a 1");
+        // mise à jour sauce base de donnée
+        Sauce.updateOne(
+          { _id: req.params.id }, { $inc: { likes: 1 }, $push: { usersLiked: req.body.userId } }
+        )
+          .then(() => (201).json({ message: "Like ajouté!" }))
+          .catch((error) => res.status(400).json({ error }));
+      };
+      // likes = 0 (likes neutre = 0)
+      if (sauce.usersLiked.includes(req.body.userId) && req.body.like === 0) {
+        console.log("userId et like est égal à 0");
+        // mise à jour sauce base de donnée
+        Sauce.updateOne(
+          { _id: req.params.id }, { $inc: { likes: -1 }, $pull: { usersLiked: req.body.userId } }
+        )
+          .then(() => (201).json({ message: "Like supprimé!" }))
+          .catch((error) => res.status(400).json({ error }));
+      };
+
+
+    })
+    .catch((error) => res.status(404).json({ error }));
+
+};
