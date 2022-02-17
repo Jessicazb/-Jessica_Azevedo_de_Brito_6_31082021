7cee53784d1ffec57e1b7cb7d52bfb52ad1acc4e
Jéssica Azevedo <jessica.azdo@gmail.com>
2022-02-17 14:27:46 +0100

    ajout du projet 6

---
+++ backend/middleware/rate-limit.js
@@ -0,0 +1,11 @@
+const rateLimit  = require('express-rate-limit');
+
+const  limiteur  =  rateLimit ({ 
+	windowMs : 2  *  60  *  1000 ,  
+	max : 3 ,  
+	standardHeaders : true ,  
+	legacyHeaders : false ,  
+    message: 'Trop de tentatives de connexion. Compte bloqué pour 15 minutes.'
+})
+
+module.exports = { limiteur }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const validatePassword = require ('../middleware/password');
+const tentativeConnexion = require ('../middleware/rate-limit');
 
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', validatePassword, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', tentativeConnexion.limiteur, userCtrl.login);
 
 module.exports = router;
